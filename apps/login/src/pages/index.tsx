5298be9a45511c7f723e5e557f8c0686b5277b66
gianluca <perronegian>
2022-05-02 11:02:31 +0100

    add reference to env var

--- apps/login/src/pages/index.tsx
+++ apps/login/src/pages/index.tsx
@@ -1,10 +1,14 @@
 import { Button } from 'shared';
 
 export default function Home() {
+  const loginTitle = () => {
+    return process.env.NEXT_PUBLIC_LOGIN_TITLE;
+  };
+
   return (
     <div>
       <Button />
-      <p>{process.env.TITLE || 'LOGIN'}</p>
+      <p>{loginTitle()}</p>
     </div>
   );
 }
